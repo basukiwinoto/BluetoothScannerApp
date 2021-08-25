9f26d12aff546fde0d9237813bafcc9244803010
Basuki Winoto <baw942@g.harvard.edu>
2021-08-24 21:48:57 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+// import App from './App';
+import App from './src/BluetoothScanner'
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
---
+++ src/BluetoothScanner.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, Button, PermissionsAndroid } from 'react-native';
+import { BleManager } from 'react-native-ble-plx';
+
+export const manager = new BleManager();
+
+const requestPermission = async () => {
+  const granted = await PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION, {
+      title: "Request for Location Permission",
+      message: "Bluetooth Scanner requires access to Fine Location Permission",
+      buttonNeutral: "Ask Me Later",
+      buttonNegative: "Cancel",
+      buttonPositive: "OK"
+    }
+  );
+  return (granted === PermissionsAndroid.RESULTS.GRANTED);
+}
+
+// BlueetoothScanner does:
+// - access/enable bluetooth module
+// - scan bluetooth devices in the area
+// - list the scanned devices
+const BluetoothScanner = () => {
+  const [logData, setLogData] = useState([]);
+  const [logCount, setLogCount] = useState(0);
+  const [scannedDevices, setScannedDevices] = useState({});
+  const [deviceCount, setDeviceCount] = useState(0);
+
+  useEffect(() => {
+    manager.onStateChange((state) => {
+      const subscription = manager.onStateChange(async (state) => {
+        console.log(state);
+        const newLogData = logData;
+        newLogData.push(state);
+        await setLogCount(newLogData.length);
+        await setLogData(newLogData);
+        subscription.remove();
+      }, true);
+      return () => subscription.remove();
+    });
+  }, [manager]);
+
+  return (
+    <View style={{flex:1, padding:10}}>
+      <View style={{flex:1, padding:10}}>
+        <Text style={{fontWeight: "bold"}}>Bluetooth Log ({logCount})</Text>
+        <FlatList
+          data={logData}
+          renderItem={({item}) => {
+            return (<Text>{item}</Text>)
+          }}
+        />
+        <Button
+          title="Turn On Bluetooth"
+          onPress={async () => {
+            const btState = await manager.state()
+            if (btState!=="PoweredOn") {
+              await manager.enable();
+            } else {
+              await manager.disable();
+            }
+            return (true);
+          }}
+        />
+      </View>
+
+      <View style={{flex:2, padding:10}}>
+        <Text style={{fontWeight: "bold"}}>Scanned Devices ({deviceCount})</Text>
+        <FlatList
+          data={Object.values(scannedDevices)}
+          renderItem={({item}) => {
+            return (<Text>{`${item.name} (${item.id})`}</Text>)
+          }}
+        />
+        <Button
+          title="Scan Devices"
+          onPress={async () => {
+            const permission = await requestPermission();
+            if (permission) {
+              manager.startDeviceScan(null, null, async (error, device) => {
+                  if (error) {
+                    console.log(error);
+                      // Handle error (scanning will be stopped automatically)
+                      return
+                  }
+
+                  if (device) {
+                    console.log(`${device.name} (${device.id})}`);
+                    const newScannedDevices = scannedDevices;
+                    newScannedDevices[device.id] = device;
+                    await setDeviceCount(Object.keys(newScannedDevices).length);
+                    await setScannedDevices(scannedDevices);
+                  }
+              });
+            }
+            return (true);
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default BluetoothScanner;
